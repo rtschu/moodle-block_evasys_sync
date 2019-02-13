9863d6ce9615e47020bd66d2c190cbe671e1ffee
r.tschu <robin.tschudi@gmail.com>
2019-02-13 18:24:32 +0100

    Made strings load from language files

--- amd/src/post_dialog.js
+++ amd/src/post_dialog.js
@@ -1,26 +1,36 @@
 
-define(['jquery', 'core/notification'], function($, notification) {
+define(['jquery', 'core/notification', 'core/str'], function($, notification, str) {
     var show_dialog_success = function() {
-        notification.alert("Evaluation erfolgreich",
-            "Sie haben die Evaluation erfolgreich beantragt. <br />" +
-            "!!!DIE EVALUATION HAT NOCH NICHT BEGONNEN!!! <br />" +
-            "Sie müssen allerdings nichts weiter tun, " +
-            "ihr Evaluationsbeauftragter wird nun nach dem Verfahren ihres Fachbereichs weiter verfahren.",
-            "Verstanden");
+        str.get_strings([
+            {'key' : 'title_success', component: 'block_evasys_sync'},
+            {'key' : 'content_success', component: 'block_evasys_sync'},
+            {'key' : 'confirm_box', component: 'block_evasys_sync'},
+        ]).done(function(s) {
+                notification.alert(s[0], s[1], s[2]);
+            }
+        ).fail(notification.exception);
     };
 
     var show_dialog_up_to_date = function() {
-        notification.alert("Evaluation bereits beantragt",
-            "Ihr Evaluationsbeauftragter hat bereist einen Auftrag zum durchführen der Evaluation von ihnen erhalten. <br />" +
-            "Für Fragen zum Status ihrer Evaluation kontaktieren sie bitte ihren Evaluationsbeauftragten.",
-            "Verstanden");
+        str.get_strings([
+            {'key' : 'title_uptodate', component: 'block_evasys_sync'},
+            {'key' : 'content_uptodate', component: 'block_evasys_sync'},
+            {'key' : 'confirm_box', component: 'block_evasys_sync'},
+        ]).done(function(s) {
+                notification.alert(s[0], s[1], s[2]);
+            }
+        ).fail(notification.exception);
     };
 
     var show_dialog_failure = function() {
-        notification.alert("Evaluation nicht beauftragt",
-            "Leider konnten keine Einladungen versentdet werden. <br />" +
-            "Bitte wenden sie sich an den Support.",
-            "Verstanden");
+        str.get_strings([
+            {'key' : 'title_failure', component: 'block_evasys_sync'},
+            {'key' : 'content_failure', component: 'block_evasys_sync'},
+            {'key' : 'confirm_box', component: 'block_evasys_sync'},
+        ]).done(function(s) {
+                notification.alert(s[0], s[1], s[2]);
+            }
+        ).fail(notification.exception);
     };
 
     return {
