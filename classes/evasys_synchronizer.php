834a8bcc4d5d2c3775e75656d0970110a4bc63c2
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-10-11 13:59:42 +0200

    fix timezone display

--- classes/evasys_synchronizer.php
+++ classes/evasys_synchronizer.php
@@ -303,9 +303,9 @@ class evasys_synchronizer {
             "hinzugefügt oder der Zeitraum angepasst. Dies ist ggf. unten angegeben.\r\n\r\n";
 
         $startdate = new \DateTime('@' . $dates["start"], \core_date::get_server_timezone_object());
-        $formattedstartdate = $startdate->format('d.m.Y H:i:s');
+        $formattedstartdate = userdate($startdate->getTimestamp(), get_string('strftimedatetimeshort', 'langconfig'));
         $enddate = new \DateTime('@' . $dates["end"], \core_date::get_server_timezone_object());
-        $formattedenddate = $enddate->format('d.m.Y H:i:s');
+        $formattedenddate = userdate($enddate->getTimestamp(), get_string('strftimedatetimeshort', 'langconfig'));
 
         $notiftext .= "Gewünschter Evaluationszeitraum: " . $formattedstartdate . " bis " .
             $formattedenddate . $textdatechanged . "\r\n\r\n";
